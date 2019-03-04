617ba42583232b6e62a0845beb94235e61f0a53c
Carlos Cruz <sda.ocr.2018@gmail.com>
2019-03-04 16:23:40 -0600

    initial commit

---
+++ CI-CloudVision-Lambda/handler.js
@@ -0,0 +1,17 @@
+'use strict';
+
+module.exports.handler = async (event, context) => {
+  var respuesta = {
+    "message": "Go Serverless v1.0! Your function executed successfully!",
+    "input": event
+  }
+  respuesta = JSON.stringify(respuesta,null,2);
+  //console.log(respuesta)
+  // return {
+  //   statusCode: 200,
+  //   body: JSON.parse(respuesta)
+  // };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
