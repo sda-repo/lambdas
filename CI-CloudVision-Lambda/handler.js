2ffe4a114120c6f2a9822bf41fb1234380546df9
Carlos Cruz <ccruz@cion.com.mx>
2019-03-06 16:11:06 -0600

    first change into handler.js

--- CI-CloudVision-Lambda/handler.js
+++ CI-CloudVision-Lambda/handler.js
@@ -13,5 +13,5 @@ module.exports.handler = async (event, context) => {
   // };
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+  return { message: 'Cambio Go Serverless v1.0! Your function executed successfully!', event };
 };
